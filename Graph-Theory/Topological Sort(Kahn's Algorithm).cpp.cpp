8ddc8333b83bf523b79f8e44d46a0be73ee08445
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-03-13 10:51:30 +0600

    Files

--- Graph-Theory/Topological Sort(Kahn's Algorithm).cpp.cpp
+++ Graph-Theory/Topological Sort(Kahn's Algorithm).cpp.cpp
@@ -7,39 +7,38 @@ int n, m;
 
 void Kahn()
 {
-	queue<int> Q;
+   queue<int> Q;
 
-	for (int i = 1; i <= n; i++)
-		if (indeg[i] == 0)
-			Q.push(i);
-	while (!Q.empty())
-	{
-		int fr = Q.front();
-		res.push_back(fr);
-		Q.pop();
+   for (int i = 1; i <= n; i++)
+      if (indeg[i] == 0)
+         Q.push(i);
 
-		for (auto x : adj[fr])
-		{
-			indeg[x]--;
-			if (indeg[x] == 0)
-				Q.push(x);
-		}
-	}
-	cout << "Topological order:\n";
-	for (auto x : res)
-		cout << x << " ";
+   while (!Q.empty()) {
+      int fr = Q.front();
+      res.push_back(fr);
+      Q.pop();
+
+      for (auto x : adj[fr]) {
+         indeg[x]--;
+         if (indeg[x] == 0)
+            Q.push(x);
+      }
+   }
+   cout << "Topological order:\n";
+   for (auto x : res)
+      cout << x << " ";
 }
 int main()
 {
-	// freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
-	cin >> n >> m;
+   // freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+   cin >> n >> m;
 
-	for (int i = 0; i < m; i++)
-	{
-		int x, y; cin >> x >> y;
-		adj[x].push_back(y);
-		indeg[y]++;
-	}
-	Kahn();
+   for (int i = 0; i < m; i++) {
+      int x, y;
+      cin >> x >> y;
+      adj[x].push_back(y);
+      indeg[y]++;
+   }
+   Kahn();
 }
