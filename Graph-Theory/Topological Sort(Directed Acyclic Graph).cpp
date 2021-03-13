ef7a76f34b0a514c850f570e269456f5de6532cb
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-03-13 10:49:36 +0600

    Files

--- Graph-Theory/Topological Sort(Directed Acyclic Graph).cpp
+++ Graph-Theory/Topological Sort(Directed Acyclic Graph).cpp
@@ -7,33 +7,32 @@ vector<int> ts;
 
 void DFS(int nd)
 {
-	vis[nd] = true;
+   vis[nd] = true;
 
-	for(auto it : adj[nd])
-		if(!vis[it])
-			DFS(it);
-	ts.push_back(nd);
+   for (int it : adj[nd])
+      if (!vis[it])
+         DFS(it);
+   ts.push_back(nd);
 }
 int main()
 {
-	int n,e;
-	//freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
+   int n, e;
+   //freopen("input.txt","r",stdin);
+   //freopen("output.txt","w",stdout);
 
-	cin>>n>>e;
+   cin >> n >> e;
 
-	for(int i = 0;i < e; i++)
-	{
-		int x,y;
-		cin>>x>>y;
-		adj[x].push_back(y);
-	}
-	for(int i = 0; i < n; i++)
-	{
-		if(!vis[i])
-			DFS(i);
-	}
-	for(int i = ts.size()-1; i >= 0; i--)
-		cout<<ts[i]<<" ";
+   for (int i = 0; i < e; i++){
+      int x, y;
+      cin >> x >> y;
+      adj[x].push_back(y);
+   }
+
+   for (int i = 0; i < n; i++) 
+      if (!vis[i])
+         DFS(i);
+   
+   for (int i = ts.size() - 1; i >= 0; i--)
+      cout << ts[i] << " ";
 
 }
