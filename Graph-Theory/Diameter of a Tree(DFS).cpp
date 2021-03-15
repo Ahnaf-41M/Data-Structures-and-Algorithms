fcb45af2cc55e00319168af9c7acf41bdebdd368
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-03-15 10:21:52 +0600

    Files

--- Graph-Theory/Diameter of a Tree(DFS).cpp
+++ Graph-Theory/Diameter of a Tree(DFS).cpp
@@ -8,35 +8,34 @@ bool vis[mx];
 
 void dfs(int nd, int cnt)
 {
-	vis[nd] = true;
-	if (cnt > cur)
-	{
-		node = nd; cur = cnt;
-	}
-	for (int ch : adj[nd])
-		if (!vis[ch])
-			dfs(ch, cnt + 1);
+   vis[nd] = true;
+   if (cnt > cur) {
+      node = nd;
+      cur = cnt;
+   }
+   for (int ch : adj[nd])
+      if (!vis[ch])
+         dfs(ch, cnt + 1);
 }
 int main()
 {
-	// freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
-	cin >> n >> m;
+   // freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+   cin >> n >> m;
 
-	while (m--)
-	{
-		int x, y; cin >> x >> y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
-	}
-	dfs(1, 0);
-	cur = 0;
-	int st = node;
+   while (m--) {
+      int x, y; cin >> x >> y;
+      adj[x].push_back(y);
+      adj[y].push_back(x);
+   }
+   dfs(1, 0);
+   cur = 0;
+   int st = node;
 
-	for (int i = 1; i <= n; i++)
-		vis[i] = false;
-	dfs(node, 0);
+   for (int i = 1; i <= n; i++)
+      vis[i] = false;
+   dfs(node, 0);
 
-	cout << "Starting node is " << st << " and end node is " << node << endl;
-	cout << "length is " << cur << endl;
+   cout << "Starting node is " << st << " and end node is " << node << endl;
+   cout << "length is " << cur << endl;
 }
