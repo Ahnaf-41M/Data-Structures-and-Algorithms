90021803d4c18ec58f2051c103e9e2550a345265
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-04-05 16:15:51 +0600

    Files

--- Graph-Theory/Kruskals Algorithm(MST).cpp
+++ Graph-Theory/Kruskals Algorithm(MST).cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mx 100005
+#define  mx   100005
+#define  int  long long
 
 struct edge
 {
@@ -8,8 +9,8 @@ struct edge
 };
 
 edge adj[mx];
-int n, m;
-int parent[mx];
+int n, m, sum;
+int parent[mx], Rank[mx];
 
 bool cmp(edge p1, edge p2)
 {
@@ -21,35 +22,39 @@ int root(int d)
    else
       return parent[d] = root(parent[d]);
 }
-void Union(int a, int b)
+void Union(int a, int b, int wt)
 {
-   parent[a] = b;
+   if (a == b)
+      return;
+   if (Rank[a] > Rank[b]) {
+      parent[b] = a;
+      Rank[a] += Rank[b];
+   }
+   else {
+      parent[a] = b;
+      Rank[b] += Rank[a];
+   }
+   sum += wt;
 }
-int main()
+signed main()
 {
    // freopen("input.txt", "r", stdin);
    // freopen("output.txt", "w", stdout);
 
-   int sum = 0, a, b;
+   int a, b;
    cin >> n >> m;
 
    for (int i = 1; i <= n; i++)
-      parent[i] = -1;
+      parent[i] = -1, Rank[i] = 1;
    for (int i = 0; i < m; i++)
-   {
       cin >> adj[i].x >> adj[i].y >> adj[i].w;
-   }
+
    sort(adj, adj + m, cmp);
 
-   for (int i = 0; i < m; i++)
-   {
+   for (int i = 0; i < m; i++) {
       a = root(adj[i].x);
       b = root(adj[i].y);
-      if (a != b)
-      {
-         Union(a, b);
-         sum += adj[i].w;
-      }
+      Union(a, b, adj[i].w);
    }
    cout << sum << endl;
 }
