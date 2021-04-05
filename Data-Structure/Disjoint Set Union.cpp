ae69e442d7a29021707a38707f99837fea8b026a
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-04-05 16:40:13 +0600

    Files

--- Data-Structure/Disjoint Set Union.cpp
+++ Data-Structure/Disjoint Set Union.cpp
@@ -2,41 +2,53 @@
 using namespace std;
 #define mx 100005
 
-int par[mx], m, n, q;
+int par[mx], m, n, q, Rank[mx];
 
-int find(int a)
+int root(int a)
 {
-    if (par[a] == a)
+    if (par[a] == -1)
         return a;
     else
-        return par[a] = find(par[a]);
+        return par[a] = root(par[a]);
 }
 void Union(int a, int b)
 {
-    int a1 = find(a);
-    int b1 = find(b);
-    if (a1 != b1)
-        par[a1] = b1;
+    int p1 = root(a);
+    int p2 = root(b);
+
+    if (p1 == p2) return;
+
+    if (Rank[p1] > Rank[p2]) {
+        par[p2] = p1;
+        Rank[p1] += Rank[p2];
+    }
+    else{
+        par[p1] = p2;
+        Rank[p2] += Rank[p1];
+    }
 }
 int main()
 {
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
-        par[i] = i;
-    while (m--)
-    {
-        int u, v; cin >> u >> v;
+        par[i] = -1, Rank[i] = 1;
+
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
         Union(u, v);
     }
+
     cin >> q;
     while (q--)
     {
         int x, y; cin >> x >> y;
-        int p1 = find(x);
-        int p2 = find(y);
-        if (par[p1] == par[p2])
+        int p1 = root(x);
+        int p2 = root(y);
+        if (p1 == p2)
             cout << "They are in same set.\n";
         else
             cout << "They are not in the same set.\n";
