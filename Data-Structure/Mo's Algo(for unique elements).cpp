6b7441c42a102a8509180e533980eafc83e6d554
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-04-05 12:11:29 +0600

    Update Mo's Algo(for unique elements).cpp

--- Data-Structure/Mo's Algo(for unique elements).cpp
+++ Data-Structure/Mo's Algo(for unique elements).cpp
@@ -67,7 +67,7 @@ void AnswerQuery()
 }
 int main()
 {
-   ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
    cin >> n;
    rep(i, 0, n - 1) cin >> a[i];
 
