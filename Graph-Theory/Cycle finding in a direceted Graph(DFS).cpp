768f0472ef0abcdfb6c1c95246fa4e88d18db866
Ahnaf Chowdhury <mdahnaf7@gmail.com>
2021-10-20 10:46:57 +0600

    File

--- Graph-Theory/Cycle finding in a direceted Graph(DFS).cpp
+++ Graph-Theory/Cycle finding in a direceted Graph(DFS).cpp
@@ -7,59 +7,54 @@ int n, m;
 bool ok = true;
 int cycle_start, cycle_end;
 
-void DFS(int nd)
+void dfs(int nd)
 {
-	color[nd] = 1;
+   color[nd] = 1;
 
-	for (auto x : adj[nd])
-	{
-		if (!color[x])
-		{
-			parent[x] = nd;
-			DFS(x);
-		}
-		else if (color[x] == 1)
-		{
-			cycle_end = nd;
-			cycle_start = x;
-			ok = false;
-			return;
-		}
-	}
-	if (!ok)
-		return;
-	color[nd] = 2;
+   for (auto x : adj[nd]) {
+      if (!color[x]) {
+         parent[x] = nd;
+         dfs(x);
+      }
+      else if (color[x] == 1) {
+         cycle_end = nd;
+         cycle_start = x;
+         ok = false;
+         return;
+      }
+   }
+   if (!ok)
+      return;
+   color[nd] = 2;
 }
 int main()
 {
-	cin >> n >> m;
-	// freopen("input.txt","r",stdin);
-	// freopen("output.txt","w",stdout);
+   cin >> n >> m;
+   // freopen("input.txt","r",stdin);
+   // freopen("output.txt","w",stdout);
 
-	for (int i = 0; i < m; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		adj[x].push_back(y);
-	}
-	for (int i = 1; i <= n; i++)
-		if (!color[i])
-			DFS(i);
-	if (ok)
-		cout << "Graph is Acyclic!\n";
-	else
-	{
-		vector<int> Cycle;
-		Cycle.push_back(cycle_start);
+   for (int i = 0; i < m; i++) {
+      int x, y;
+      cin >> x >> y;
+      adj[x].push_back(y);
+   }
+   for (int i = 1; i <= n; i++)
+      if (!color[i])
+         dfs(i);
+   if (ok)
+      cout << "Graph is Acyclic!\n";
+   else {
+      vector<int> Cycle;
+      Cycle.push_back(cycle_start);
 
-		for (int v = cycle_end; v != cycle_start; v = parent[v])
-			Cycle.push_back(v);
-		Cycle.push_back(cycle_start);
-		reverse(Cycle.begin(), Cycle.end());
+      for (int v = cycle_end; v != cycle_start; v = parent[v])
+         Cycle.push_back(v);
+      Cycle.push_back(cycle_start);
+      reverse(Cycle.begin(), Cycle.end());
 
-		cout<<"Cycle:\n"; 
-		for (int i = 0; i < Cycle.size(); i++) cout << Cycle[i] << " ";
-		cout << endl;
-	}
+      cout << "Cycle:\n";
+      for (int x : Cycle) cout << x << " ";
+      cout << '\n';
+   }
 
 }
