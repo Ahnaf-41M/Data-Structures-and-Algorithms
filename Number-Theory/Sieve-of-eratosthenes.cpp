b99a1e6c44e409915e39ce598ae993e9610aa114
Ahnaf-41M <mdahnaf7@gmail.com>
2021-12-29 17:01:29 +0600

    File

--- Number-Theory/Sieve-of-eratosthenes.cpp
+++ Number-Theory/Sieve-of-eratosthenes.cpp
@@ -1,45 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define Max 1000000
-bool a[Max];
-
+#define MX 50000
+bool OK[MX];
 vector<int> v;
-void sieve()
-{
-    ll i,j;
-    a[0]=0,a[1]=0,a[2]=1;
 
-    for(i = 3; i <= Max; i+=2)
-        a[i]=1;
-    for(i = 3; i < sqrt(Max); i++)
-    {
-        if(a[i]==1)
-        {
-            for(j = i*i; j <= Max; j+=i)
-            {
-                a[j]=0;
-            }
-        }
-    }
+void Sieve()
+{
+    for (int i = 3; i < MX; i += 2)
+        OK[i] = 1;
+    for (int i = 3; i * i < MX; i++)
+        if (OK[i])
+            for (int j = i * i; j < MX; j += i)
+                OK[j] = 0;
+
     v.push_back(2);
-    for(i = 3; i <= Max; i+=2)
-        if(a[i])
+    for (int i = 3; i < MX; i += 2)
+        if (OK[i])
             v.push_back(i);
 }
 int main()
 {
-    ll i,j,n;
-    sieve();
-
-    cout<<v.size()<<endl;
-    cout<<"Enter a number: ";
-    cin>>n;
-    if(a[n])
-        cout<<n<<" is a prime number\n";
-    else
-        cout<<n<<" is not a prime number\n";
+    Sieve();
+    int n;
+    cin >> n;
+    if (OK[n]) cout << n << " is a prime number\n";
+    else cout << n << " is not a prime number\n";
 
     return 0;
 }
