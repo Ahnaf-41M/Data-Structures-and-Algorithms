1dcbe1fbd857457cd9d6d3b04414c6753bdddc37
Ahnaf Chowdhury <mdahnaf7@gmail.com>
2022-08-25 11:00:52 +0600

    File

---
+++ Number-Theory/Eular inclusion exclusion.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 5000005
+int phi[MAX];
+void find_phi()
+{
+    int i, j;
+    for (i = 1; i < MAX; i++)
+        phi[i] = i;
+    for (i = 2; i < MAX; i++) {
+        if (phi[i] == i) {
+            for (j = i; j < MAX; j += i) {
+                phi[j] -= phi[j] / i; //Inclusion_Exclusion Law used
+            }
+        }
+    }
+}
+int main()
+{
+    find_phi();
+    int n = 10;
+    cout << phi[n] << endl;
+
+}
